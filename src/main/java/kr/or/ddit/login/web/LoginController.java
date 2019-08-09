b1cae1613cd0ca44b905533a24c35e36e98448f9
kdh <gnsdl4879@gmail.com>
2019-08-09 17:11:12 +0900

    server side cookie 	-웹브라우저가 요청시 보낸 쿠키 정보조회 	-request.getCookies();
    	-서버가 응답생성시 클라이언트에게 새로운 쿠키를 생성하도록 지시
    	-response.addCookie(Cookie);

--- src/main/java/kr/or/ddit/login/web/LoginController.java
+++ src/main/java/kr/or/ddit/login/web/LoginController.java
@@ -3,6 +3,7 @@ package kr.or.ddit.login.web;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +34,23 @@ public class LoginController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//webapp/jsp/login.jsp --> /jsp/login.jsp
+		
+		
+		//웹브라우져가 보낸 cookie 확인
+		Cookie[] cookies = request.getCookies();
+		for(Cookie cookie : cookies) {
+			logger.debug("cookie name : {}, cookie value : {}", 
+					cookie.getName(), cookie.getValue());
+			
+		}
+		
+		//응답을 생성할때 웹브라우저에게 쿠키를 저장할 것을 지시
+
+		Cookie cookie = new Cookie("serverGen", "serverValue");
+		cookie.setMaxAge(60*60*24*7); 		//7일의 유효기간을 갖는 쿠키
+		response.addCookie(cookie);
+		
+		
 		request.getRequestDispatcher("/login/login.jsp").forward(request, response);
 	}
 
@@ -67,9 +85,13 @@ public class LoginController extends HttpServlet {
 		//사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 동일 할 경우 --> webapp/main.jsp
 		//사용자가 입력한 파라미터 정보와 db에서 조회해온 값이 다를 경우 --> webapp/login/login.jsp
 		
+		//db에 존재하지 않는 사용자 체크 --> 로그인 화면으로 이동
+		if(user == null)
+			doGet(request, response);
 		
 		
-		if(user.checkLoginValidate(userId, pass)) {
+		
+		else if(user.checkLoginValidate(userId, pass)) {
 			
 			HttpSession session = request.getSession();
 			logger.debug("session.getId() : {}", session.getId());
