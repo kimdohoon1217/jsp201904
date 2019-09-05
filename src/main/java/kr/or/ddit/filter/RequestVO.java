81d833689759b6f919b624e6e8d32693125a3d40
kdh <gnsdl4879@gmail.com>
2019-09-05 13:43:49 +0900

    "requestCounterFilter 기능변경  . key : uri --> uri + method  . RequestVo"

--- src/main/java/kr/or/ddit/filter/RequestVO.java
+++ src/main/java/kr/or/ddit/filter/RequestVO.java
@@ -6,6 +6,40 @@ public class RequestVO {
 	
 	
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((method == null) ? 0 : method.hashCode());
+		result = prime * result + uriCount;
+		return result;
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RequestVO other = (RequestVO) obj;
+		if (method == null) {
+			if (other.method != null)
+				return false;
+		} else if (!method.equals(other.method))
+			return false;
+		if (uriCount != other.uriCount)
+			return false;
+		return true;
+	}
+
+
+
+
 	public RequestVO() {
 		super();
 	}
